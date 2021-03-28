e4f5a168c349c45dcd318068d06a242da5eaf6d2
Marcel Marques <marcmatiasdev@gmail.com>
2021-03-28 19:53:08 -0300

    Changing to gridsome

---
+++ gridsome.config.js
@@ -0,0 +1,49 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+
+  siteName: 'marcmatias',
+  siteDescription: 'Esta é a minha página pessoal. Aqui você vai encontrar informações sobre minha carreira profissional acessando o card abaixo e também algumas postagens sobre tecnologia e conteúdo interessante que eu tenho estudado no menu Blog.',
+
+  siteUrl: 'https://marcmatias.github.io',
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id'
+  },
+
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: 'content/posts/*.md',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    }
+  ],
+
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs', ['gridsome-plugin-remark-youtube', { width: '100%', align: 'auto' }]
+      ]
+    }
+  }
+
+}
---
+++ src/components/Author.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="author">
+
+		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+
+		<h1 v-if="showTitle" class="author__site-title">
+			{{ $static.metadata.siteName }}
+		</h1>
+
+		<p class="author__site-description">
+			{{ $static.metadata.siteDescription }}
+		</p>
+
+		<p class="author__links">
+			<a target="_blank" href="//codepen.io/marcmatias">Codepen</a>
+			<a target="_blank" href="//github.com/marcmatias">GitHub</a>
+			<a target="_blank" href="//www.linkedin.com/in/marcel-matias-836a04208/">LinkdIn</a>
+			<a target="_blank" href="//twitter.com/marcmatias">Twitter</a>
+		</p>
+
+	</div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+	siteDescription
+  }
+}
+</static-query>
+
+<script>
+export default {
+	props: ['showTitle']
+}
+</script>
+
+<style lang="scss">
+.author {
+	margin: 0 auto;
+	max-width: 500px;
+	text-align: center;
+	padding: calc(var(--space) / 2) 0;
+
+	&__image {
+		border-radius: 100%;
+		width: 90px;
+		height: 90px;
+		margin-bottom: 1em;
+	}
+
+	&__intro {
+		opacity: .8;
+	}
+
+	&__site-title {
+		font-size: 1.5em;
+	}
+
+	&__site-description {
+		font-size: 0.9em;
+		text-align: justify;
+	}
+
+	&__links {
+		margin-top: -.5em;
+		a {
+			margin: 0 .5em;
+		}
+	}
+}
+</style>
